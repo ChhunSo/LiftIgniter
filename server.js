3d70b9c3e8eae9c38625d700239ca70975327df8
ChhunSo <chhun.so@gmail.com>
2018-10-28 20:33:04 -0700

    change server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const http = require('http');
+var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 8080;
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 5000;
 
-const server = http.createServer((req,res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
+app.get('/', function(req,res) {
+	res.send('index');
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running on ${PORT}`);
-});
+
+app.listen(port, function(){
+	console.log('running on localhost:' + port);
+})
